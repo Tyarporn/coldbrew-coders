e40e3326b492696191e7c56b5d93519f42a84462
Ty Arpornsuksant <tyler.arporn741@gmail.com>
2022-10-01 16:29:51 -0400

    added server recognition

--- bot_dev/main.py
+++ bot_dev/main.py
@@ -10,6 +10,10 @@ client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
 
 client.run(TOKEN)
