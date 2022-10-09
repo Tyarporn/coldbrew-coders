f6c6fd95ad26439d199aacf71978c8dc0626b898
Ty Arpornsuksant <tyler.arporn741@gmail.com>
2022-10-09 17:23:14 -0400

    added message functionality

--- ty_testing/bot_dev/ty-bot.py
+++ ty_testing/bot_dev/ty-bot.py
@@ -1,12 +1,15 @@
 import os
 
 import discord
+import random
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
 
 @client.event
 async def on_ready():
@@ -22,5 +25,23 @@ async def on_member_join(member):
     await member.dm_channel.send(
         f'Hi {member.name}, welcome to the Coldbrew Cafe!'
     )
+
+@client.event
+async def on_message(message): 
+    print(message)
+    print(message.author, client.user, message.content, message.channel)
+
+    if message.author == client.user: # check if sender is bot itself (avoids recursion)
+        return
+
+    brew_bot_responses = [
+        'Hi, how can I help you?',
+        'Hey, what can I do for you today?',
+        'Hello! How can I serve you?'
+    ]
+
+    if message.content == '!coffee':
+        response = random.choice(brew_bot_responses)
+        await message.channel.send(response)
 
 client.run(TOKEN)
