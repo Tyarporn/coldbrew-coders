c8bf7277693fd96350a8510c34e2afd73fd373ec
Cameron Vong <cameronvong@gmail.com>
2022-10-09 18:00:05 -0400

    Added message functionality to bot

--- cameron_testing/cameron-bot.py
+++ cameron_testing/cameron-bot.py
@@ -1,6 +1,6 @@
 # bot.py
 import os
-
+import random
 import discord
 from dotenv import load_dotenv
 
@@ -21,5 +21,24 @@ async def on_ready():
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    shout = [
+        'SCREAM',
+        'YELL',
+        (
+            'AHHHHHHHH'
+            'Ok cool'
+        ),
+    ]
+
+    if message.content == '!syzygy':
+        response = random.choice(shout)
+        await message.channel.send(response)
 
 client.run(TOKEN)
