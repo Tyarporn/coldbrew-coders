b46ae2c09ba9d4af9dba1a21f6d274bd753b3daa
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2022-10-11 15:04:14 -0400

    added message handling

--- shashanka_testing/shank-bot.py
+++ shashanka_testing/shank-bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import random
 
 from dotenv import load_dotenv
 
@@ -20,5 +21,49 @@ async def on_ready():
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
+    # members = '\n - '.join([member.name for member in guild.members]) prints guild members
+    # print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to the Discord Server!'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+    send_message = """
+    Hello, my name is Brewmeister and I was developed by Shashanka to help automate away responsibilities humans face on a daily basis.
+    """
+
+    stonny_message = """
+    Gender: Male
+    Ethnicity: Chinese
+    Height: Taller Than Me
+    Weight: Skinny Muscular
+    Hates: Most things
+    Likes: Playing Ekko
+    """
+
+    if message.content == 'info!':
+        response = send_message
+        await message.channel.send(response)
+
+    if message.content == 'Stoneson Liu!':
+        response = stonny_message
+        await message.channel.send(response)
 
 client.run(TOKEN)
