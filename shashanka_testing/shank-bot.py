daadf4d2327f3d447ce38b3afe3799c67d7f0ba9
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2022-10-10 23:44:05 -0400

    Added boilerplate code to support connection

--- shashanka_testing/shank-bot.py
+++ shashanka_testing/shank-bot.py
@@ -5,11 +5,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
 
 client.run(TOKEN)
