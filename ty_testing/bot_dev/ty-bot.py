46616a746e26d0f54ab4c298d4a0550b4d9a953f
Ty Arpornsuksant <tyler.arporn741@gmail.com>
2022-10-11 12:35:58 -0400

    changed env path from demo-repo3 to coldbrew-coders

--- ty_testing/bot_dev/ty-bot.py
+++ ty_testing/bot_dev/ty-bot.py
@@ -27,7 +27,7 @@ async def on_member_join(member):
     )
 
 @client.event
-async def on_message(message): 
+async def on_message(message):
     print(message)
     print(message.author, client.user, message.content, message.channel)
 
