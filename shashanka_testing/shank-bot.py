42ed91f95c52918f168038338333e17c6f2f8bbd
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2022-11-06 16:40:41 -0500

    added help info

--- shashanka_testing/shank-bot.py
+++ shashanka_testing/shank-bot.py
@@ -37,74 +37,37 @@ async def on_member_join(member):
 
 
 @brewmeister.command(
+	help="Uses come crazy logic to determine if pong is actually the correct value or not.",
+	brief="Prints pong back to the channel."
 )
 async def ping(ctx):
-    await ctx.channel.send("pong!")
+	await ctx.channel.send("pong")
 
-@brewmeister.command()
+@brewmeister.command(
+	help="Uses some crazy logic to determine who I am.",
+	brief="Returns back who I am."
+)
 async def info(ctx):
 	await ctx.channel.send("Hello, my name is Brewmeister and I was developed by Shashanka to help automate away responsibilities humans face on a daily basis.")
 
-# prints out arguments that were passed to it
-@brewmeister.command()
+@brewmeister.command(
+	help="Looks like you need some help.",
+	brief="Prints the list of values back to the channel."
+)
 async def printArgs(ctx, *args):
     response = ""
-    
     for arg in args:
 	    response = response + " " + arg
 
     await ctx.channel.send('Welcome {response} alumni!')
 
 
-# @brewmeister.event
-# async def on_message(message):
-#     if message.author == brewmeister.user:
-#         return
+@brewmeister.event
+async def on_message(message):
+	if message.content == "hello":
+		await message.channel.send("pies are better than cakes. change my mind.")
 
+	await brewmeister.process_commands(message)
 
-#     brooklyn_99_quotes = [
-#         'I\'m the human form of the 💯 emoji.',
-#         'Bingpot!',
-#         (
-#             'Cool. Cool cool cool cool cool cool cool, '
-#             'no doubt no doubt no doubt no doubt.'
-#         ),
-#     ]
-#     send_message = """
-#     Hello, my name is Brewmeister and I was developed by Shashanka to help automate away responsibilities humans face on a daily basis.
-#     """
-
-#     stonny_message = """
-#     Gender: Male
-#     Ethnicity: Chinese
-#     Height: Taller Than Me
-#     Weight: Skinny Muscular
-#     Hates: Most Things
-#     Likes: Playing Ekko
-#     """
-
-#     stock_ticker = """
-#     APPL : $150.09
-#     """
-
-#     sis_message = """
-#     Hello Sharbani, this is Shashanka's bot greeting you!
-#     """
-
-#     if message.content == 'info!':
-#         response = send_message
-#         await message.channel.send(response)
-
-#     if message.content == 'Stoneson Liu!':
-#         response = stonny_message
-#         await message.channel.send(response)
-
-#     if message.content == 'APPL!':
-#         response = stock_ticker
-#         await message.channel.send(response)
-    
-#     if message.content == 'Sharbani!':
-#         response = sis_message
-#         await message.channel.send(response)
 
 brewmeister.run(TOKEN)
