eeaeb4b8b7161158c2d08df97d67955356a1f9a6
Cameron Vong <cameronvong@gmail.com>
2022-11-10 10:25:46 -0500

    Update cameron-bot.py
    changed prefixes for no bug testing conflict

--- cameron_testing/cameron-bot.py
+++ cameron_testing/cameron-bot.py
@@ -39,18 +39,18 @@ async def on_message(message):
     priceResonse = "The current price is: "
     changeResponse = "The change of this stock over the last year is: "
 
-    if message.content == '!help':
+    if message.content == '$help':
         response = helpResponse
         await message.channel.send(response)
     
-    elif message.content == "!price":
+    elif message.content == "$price":
         msft = yf.Ticker("MSFT")
         response = msft.info
     
-    elif message.content == "!change":
+    elif message.content == "$change":
         response = changeResponse
 
-@client.command(name='!TEST')
+@client.command(name='$TEST')
 async def test(ctx):
     print("Test!")
     await ctx.send("Test successful!")
