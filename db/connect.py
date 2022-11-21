861a80b5ff6ba064ca81ac7a57c5983d6b169722
Ty Arpornsuksant <tyler.arporn741@gmail.com>
2022-11-20 20:41:21 -0500

    added query function

--- db/connect.py
+++ db/connect.py
@@ -4,26 +4,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-username = os.getenv('db_username')
-password = os.getenv('db_password')
+USERNAME = os.getenv('db_username') # get username and password
+PASSWORD = os.getenv('db_password')
+CONNECTION_STRING = f'mongodb+srv://{USERNAME}:{PASSWORD}@cluster0.wux8g.mongodb.net/?retryWrites=true&w=majority' # get connection string
+client = MongoClient(CONNECTION_STRING) # establish client
 
-
-def get_database():
- 
-   # Provide the mongodb atlas url to connect python to mongodb using pymongo
-
-   CONNECTION_STRING = f'mongodb+srv://{username}:{password}@cluster0.wux8g.mongodb.net/?retryWrites=true&w=majority'
- 
-   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-   client = MongoClient(CONNECTION_STRING)
- 
+def query_id():
    # Create the database for our example (we will use the same database throughout the tutorial
-   print(client)
-   return client['coldbrew_db']
+   mydb = client["coldbrew_coders"] # get specific database
+   mycol = mydb["bot_ids"] # get specific column
+#    print(mycol)
+   myquery = { "bot_id": "0460" } # find document with bot id like 0460
+   mydoc = mycol.find(myquery)
+
+   for x in mydoc:
+    print(x) 
+   return 
 
   
 # This is added so that many files can reuse the function get_database()
 if __name__ == "__main__":   
-  
-   # Get the database
-   dbname = get_database()
+   query_id()
