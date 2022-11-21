386e8c85abe1f3001f7089cc3726ecde97359889
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2022-11-21 16:06:01 -0500

    linting errors resolved

--- db/connect.py
+++ db/connect.py
@@ -2,26 +2,23 @@ from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
 
-USERNAME = os.getenv('db_username') # get username and password
+load_dotenv()
+USERNAME = os.getenv('db_username')
 PASSWORD = os.getenv('db_password')
-CONNECTION_STRING = f'mongodb+srv://{USERNAME}:{PASSWORD}@cluster0.wux8g.mongodb.net/?retryWrites=true&w=majority' # get connection string
-client = MongoClient(CONNECTION_STRING) # establish client
+CONNECTION_STRING = f'mongodb+srv://{USERNAME}:{PASSWORD}@cluster0.wux8g.mongodb.net/?retryWrites=true&w=majority'
+client = MongoClient(CONNECTION_STRING)
+
 
 def query_id():
-   # Create the database for our example (we will use the same database throughout the tutorial
-   mydb = client["coldbrew_coders"] # get specific database
-   mycol = mydb["bot_ids"] # get specific column
-#    print(mycol)
-   myquery = { "bot_id": "0460" } # find document with bot id like 0460
-   mydoc = mycol.find(myquery)
+    mydb = client["coldbrew_coders"]
+    mycol = mydb["bot_ids"]
+    myquery = {"bot_id": "0460"}
+    mydoc = mycol.find(myquery)
 
-   for x in mydoc:
-    print(x) 
-   return 
+    for x in mydoc:
+        print(x)
 
-  
-# This is added so that many files can reuse the function get_database()
-if __name__ == "__main__":   
-   query_id()
+
+if __name__ == "__main__":
+    query_id()
