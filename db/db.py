4bba0b71b59be73b192f5639b3552c8b7f127cc7
Ty Arpornsuksant <tyler.arporn741@gmail.com>
2022-11-20 19:51:05 -0500

    added fetch_bot_description

--- db/db.py
+++ db/db.py
@@ -15,5 +15,10 @@ def fetch_bot_id():
     """
     A function to return all discord bot ids in the data store.
     """
-    return {"Brewbot": 1025852605257220106, "Brewmeister": 1234}
+    return {"Brewbot": 1025852605257220106, "Brewmeister": 1234, "Stonkster": 2345, "CricSco": 3456}
 
+def fetch_bot_description():
+    """
+    A function to return all discord bot descriptions in the data store.
+    """
+    return {"Brewbot": "Discord bot made for CS-UY 4513 Software Engineering INET", "Brewmeister": "Shashanka's tutorial bot", "Stonkster":"Yeehaw Cameron's cooking up a bot", "CricSco":"Get the latest cricket scores and fixtures!"}
