3c5dc66e5c82c0e331c7cac1f1cfe5aa483005fb
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2022-11-23 13:58:52 -0500

    created first test for brewmeister endpoint

--- bot_development/brewmeister_dev/test_endpoints.py
+++ bot_development/brewmeister_dev/test_endpoints.py
@@ -1,14 +1,9 @@
-from header_files import *
+import pytest
 import endpoints as ep
 
-# TEST_CLIENT = ep.app.test_client()
+TEST_CLIENT = ep.app.test_client()
 
-# def test_hello():
-#     """
-#     See if Hello works.
-#     """
-#     resp_json = TEST_CLIENT.get(ep.HELLO).get_json()
-#     assert isinstance(resp_json[ep.MESSAGE], str)
-
-# def testCryptoPrice():
+def testCryptoPrice():
+    resp_json = TEST_CLIENT.get(ep.CRYPTOPRICE).get_json()
+    assert isinstance(resp_json[ep.PRICE], float)
     
