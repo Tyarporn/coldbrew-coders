032ad9b877f857245bca6d3a1b27dfa7c166cb47
Ty Arpornsuksant <tyler.arporn741@gmail.com>
2022-11-27 22:46:05 -0500

    added test for coin api and moved skip to other test

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -17,14 +17,18 @@ def coin_api_value():
    test_value = "BTC"
    return test_value
 
-def test_review_api_call(review_api_value):
+@pytest.mark.skip(reason="not implemented yet in main endpoints file. Will add from bot testing later")
+def testCryptoPrice():
+    resp_json = TEST_CLIENT.get(ep.CRYPTOPRICE).get_json()
+    assert isinstance(resp_json[ep.PRICE], dict)
+
+def test_coin_api_call(review_api_value):
     resp_json = TEST_CLIENT.get(ep.MOVIEREVIEW).get_json()
     assert isinstance(resp_json[ep.MOVIEREVIEWRESPONSE], dict)
 
 def test_create_bot():
     resp_json = TEST_CLIENT.get(ep.CREATE).get_json()
     assert isinstance(resp_json[ep.CREATEBOTRESPONSE], str)
-
 
 def test_list_bot():
     resp_json = TEST_CLIENT.get(ep.LIST).get_json()
@@ -45,8 +49,7 @@ def test_rate_bot():
 def test_review_bot():
     resp_json = TEST_CLIENT.get(ep.REVIEWBOT).get_json()
     assert isinstance(resp_json[ep.REVIEWBOTRESPONSE], str)
-    
-@pytest.mark.skip(reason="no way of currently testing this")
+
 def test_update_bot():
     resp_json = TEST_CLIENT.get(ep.UPDATEBOT).get_json()
     assert isinstance(resp_json[ep.UPDATEBOTRESPONSE], str)
