6b82c5c41de6e4e9ee013f5e2d9aef9b91698f78
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2022-11-29 16:28:19 -0500

    added sample test in bot_info tests

--- db/tests/test_bot_info.py
+++ db/tests/test_bot_info.py
@@ -6,3 +6,10 @@ import db.bot_info as bi
 
 
 RUNNING_ON_CICD_SERVER = os.environ.get('CI', False)
+
+
+def test_bot_id(bot_name):
+    bot_ids = bi.get_bot_id()
+    assert isinstance(bot_ids, dict)
+    assert len(bot_ids) == 4
+    
