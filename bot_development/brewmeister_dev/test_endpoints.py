91c4507ffa92217382ce8eea3dfc28038ad6bae6
Ty Arpornsuksant <tyler.arporn741@gmail.com>
2022-12-11 16:40:04 -0500

    fixed flak8 errors in brewmeister

--- bot_development/brewmeister_dev/test_endpoints.py
+++ bot_development/brewmeister_dev/test_endpoints.py
@@ -1,9 +1,9 @@
-import pytest
+# import pytest
 import endpoints as ep
 
 TEST_CLIENT = ep.app.test_client()
 
+
 def testCryptoPrice():
     resp_json = TEST_CLIENT.get(ep.CRYPTOPRICE).get_json()
     assert isinstance(resp_json[ep.PRICE], dict)
-    
