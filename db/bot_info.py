780b288de47fab6a2a18367002d45fe55315f5ba
Cameron Vong <cameronvong@gmail.com>
2022-12-11 12:27:31 -0500

    Update bot_info.py
    added db connectivity

--- db/bot_info.py
+++ db/bot_info.py
@@ -6,6 +6,10 @@ import db.db_connect as dbc
 
 COLLECTION = 'bot_ids'
 BOT_NAME = 'name'
+BOT_ID = 'id'
+BOT_DESC = 'desc'
+REQUIRED_FLDS = [BOT_NAME, BOT_ID, BOT_DESC]
+
 
 def bot_exists(name):
     dbc.connect_db()
@@ -19,27 +23,35 @@ def get_bot_ids():
     """
     A function to return all discord bot ids in the data store.
     """
-    return {"Brewbot": 1025852605257220106,
-            "Brewmeister": 1029079767443591220,
-            "Stonkster": 1028785870163157082,
-            "CricSco": 1039923876039237743
-            }
-
+    # return {"Brewbot": 1025852605257220106,
+    #         "Brewmeister": 1029079767443591220,
+    #         "Stonkster": 1028785870163157082,
+    #         "CricSco": 1039923876039237743
+    #         }
+    dbc.connect_db()
+    raw_data = dbc.fetch_all_as_dict('id', COLLECTION)
+    return list(raw_data.keys())
 
 def get_bot_names():
     """
     A funnction to return all the names of the bots in the data store.
     """
-    return ["Brewmeister", "Brewbot", "CrisCo", "Stonkster"]
+    # return ["Brewmeister", "Brewbot", "CrisCo", "Stonkster"]
+    dbc.connect_db()
+    return dbc.fetch_all_as_dict('name', COLLECTION)
+    return list(raw_data.keys())
 
 
 def get_bot_descs():
     """
     A function to return all discord bot descriptions in the data store.
     """
-    return {"Brewbot": """Discord bot made for
-                        CS-UY 4513 Software Engineering INET""",
-            "Brewmeister": "Shashanka's tutorial bot",
-            "Stonkster": "Yeehaw Cameron's cooking up a bot",
-            "CricSco": "Get the latest cricket scores and fixtures!"
-            }
+    # return {"Brewbot": """Discord bot made for
+    #                     CS-UY 4513 Software Engineering INET""",
+    #         "Brewmeister": "Shashanka's tutorial bot",
+    #         "Stonkster": "Yeehaw Cameron's cooking up a bot",
+    #         "CricSco": "Get the latest cricket scores and fixtures!"
+    #         }
+    dbc.connect_db()
+    raw_data = dbc.fetch_all_as_dict('desc', COLLECTION)
+    return list(raw_data.keys())
