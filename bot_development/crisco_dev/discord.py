60c29ff61d844c9776fab5bdc1574fa999603d8a
Tahmeed Chowdhury <31861714+chowdhurytahmeed@users.noreply.github.com>
2022-12-11 22:50:55 -0500

    Delete discord.py

--- bot_development/crisco_dev/discord.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-# client = discord.Client()
-# client.run(TOKEN)
-# @ Tahmeed. I commented this out bc u dont do anything with it
