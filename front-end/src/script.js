1cb26352859631c90e005bc8b009a84d42eb645b
Tahmeed Chowdhury <31861714+chowdhurytahmeed@users.noreply.github.com>
2023-02-12 19:18:46 -0500

    Add files via upload

---
+++ front-end/src/script.js
@@ -0,0 +1,91 @@
+import React from 'react'
+
+import styles from './i-phone13pro-max5.module.css'
+
+const IPhone13ProMax5 = (props) => {
+  return (
+    <div>
+      <div className={styles['i-phone13pro-max5']}>
+        <img
+          src="/playground_assets/rectangle12066-hqzs-200h.png"
+          alt="Rectangle12066"
+          className={styles['rectangle1']}
+        />
+        <img
+          src="/playground_assets/leaderboard2067-5t48.svg"
+          alt="leaderboard2067"
+          className={styles['leaderboard']}
+        />
+        <img
+          src="/playground_assets/coolicon2068-rh9.svg"
+          alt="coolicon2068"
+          className={styles['coolicon']}
+        />
+        <img
+          src="/playground_assets/language2069-dj94.svg"
+          alt="language2069"
+          className={styles['language']}
+        />
+        <img
+          src="/playground_assets/coolicon2070-x43n.svg"
+          alt="coolicon2070"
+          className={styles['coolicon1']}
+        />
+        <div className={styles['payments']}>
+          <div className={styles['group']}>
+            <img
+              src="/playground_assets/vectori207-tner.svg"
+              alt="VectorI207"
+              className={styles['vector']}
+            />
+          </div>
+        </div>
+        <span className={styles['text']}>
+          <span>Coldbrew Coders</span>
+        </span>
+        <img
+          src="/playground_assets/rectangle22843-pqjsv-200h.png"
+          alt="Rectangle22843"
+          className={styles['rectangle2']}
+        />
+        <img
+          src="/playground_assets/rectangle43234-l7i8-200h.png"
+          alt="Rectangle43234"
+          className={styles['rectangle4']}
+        />
+        <img
+          src="/playground_assets/rectangle63471-s2ft-200h.png"
+          alt="Rectangle63471"
+          className={styles['rectangle6']}
+        />
+        <img
+          src="/playground_assets/rectangle73471-qc6f-200h.png"
+          alt="Rectangle73471"
+          className={styles['rectangle7']}
+        />
+        <img
+          src="/playground_assets/rectangle32846-hjyd-200h.png"
+          alt="Rectangle32846"
+          className={styles['rectangle3']}
+        />
+        <img
+          src="/playground_assets/rectangle53235-9teh-200h.png"
+          alt="Rectangle53235"
+          className={styles['rectangle5']}
+        />
+        <img
+          src="/playground_assets/ellipse52847-cz4k-200h.png"
+          alt="Ellipse52847"
+          className={styles['ellipse5']}
+        />
+        <img
+          src="/playground_assets/ellipse63236-l3fs-200h.png"
+          alt="Ellipse63236"
+          className={styles['ellipse6']}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default IPhone13ProMax5
