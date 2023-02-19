24ca5ce3f96d9d33d4081b7feef57e4a23008d0d
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-02-18 22:36:06 -0500

    fixed deploy.sh

--- back-end/server/deploy.sh
+++ back-end/server/deploy.sh
@@ -2,6 +2,4 @@ export PATH=/home/ec2-user/.local/bin:$PATH
 export PYTHONPATH=/home/ec2-user/coldbrew-coders/back-end/server:$PYTHONPATH
 export PYTHONPATH=/home/ec2-user/coldbrew-coders/back-end:$PYTHONPATH
 export PYTHONPATH=/home/ec2-user/coldbrew-coders/back-end/db:$PYTHONPATH
-cd 
-cd home/ec2-user/coldbrew-coders/back-end/server/
 bash local.sh
