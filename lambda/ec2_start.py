dcb859107c09f4ae9ed2f7b3442324e031d4bfb2
Ty Arpornsuksant <tyler.arporn741@gmail.com>
2023-02-28 12:20:02 -0500

    added lambda functions for start and stop

---
+++ lambda/ec2_start.py
@@ -0,0 +1,11 @@
+import json
+import boto3
+
+region = 'us-east-1'
+instances = ['i-07f9ceb5b274160a8']
+ec2 = boto3.client('ec2', region_name=region)
+
+def lambda_handler(event, context):
+    ec2.start_instances(InstanceIds=instances)
+    for i in range(len(instances)):
+        print('Started instance ' + str(i) )
