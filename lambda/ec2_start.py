028cdb5b68f66696ec1ed02fec1e4eb395f8e4b8
Ty Arpornsuksant <tyler.arporn741@gmail.com>
2023-03-01 11:17:18 -0500

    added auto deploy for lambda funtions

--- lambda/ec2_start.py
+++ lambda/ec2_start.py
@@ -5,6 +5,7 @@ region = 'us-east-1'
 instances = ['i-07f9ceb5b274160a8']
 ec2 = boto3.client('ec2', region_name=region)
 
+# starts remote server
 def lambda_handler(event, context):
     ec2.start_instances(InstanceIds=instances)
     for i in range(len(instances)):
--- lambda/ec2_stop.py
+++ lambda/ec2_stop.py
@@ -5,6 +5,7 @@ region = 'us-east-1'
 instances = ['i-07f9ceb5b274160a8']
 ec2 = boto3.client('ec2', region_name=region)
 
+# stops remote server
 def lambda_handler(event, context):
     ec2.stop_instances(InstanceIds=instances)
     for i in range(len(instances)):
