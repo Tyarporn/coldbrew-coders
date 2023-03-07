54384cc862f217c2772343d6063a8c8d0b3ea9bb
Ty Arpornsuksant <tyler.arporn741@gmail.com>
2023-03-07 01:09:28 -0500

    isolated start lambda to test

--- lambda/ec2_start.py
+++ lambda/ec2_start.py
@@ -1,5 +1,6 @@
 import json
 import boto3
+import time
 
 region = 'us-east-1'
 instances = ['i-07f9ceb5b274160a8']
@@ -8,5 +9,6 @@ ec2 = boto3.client('ec2', region_name=region)
 # starts remote server
 def lambda_handler(event, context):
     ec2.start_instances(InstanceIds=instances)
+    time.sleep(30)
     for i in range(len(instances)):
         print('Started instance ' + str(i) )
