432bd2884775a13a46de7a56327fbd6dfaed89df
Ty Arpornsuksant <tyler.arporn741@gmail.com>
2023-03-23 14:35:30 -0400

    added pytest-cov to reqs. common.mk works now

--- common.mk
+++ common.mk
@@ -1,5 +1,6 @@
 LINTER = flake8
 PYTESTFLAGS = -vv --verbose --tb=short --cov=$(PKG) --cov-branch --cov-report term-missing
+# PYTESTFLAGS = -vv --verbose --tb=short  --cov-report term-missing
 
 FORCE:
 
