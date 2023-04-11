58badd9521bf80f3c9801ef6a5c77f3ea8019f1c
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-04-11 13:17:05 -0400

    renamed login form test to more apprpiate name

--- server/tests/test_login.py
+++ server/tests/test_login.py
@@ -4,8 +4,7 @@ uname = 'shanksauce315'
 pword = 'test12345'
 response = requests.post('http://127.0.0.1:8080/loginForm',
                          json={'username': uname, 'password': pword})
-# print(response.json())
 access_token = response.json()['access_token']
-# headers = {'Authorization': f'Bearer {access_token}'}
-# response = requests.get('http://127.0.0.1:8080/protected', headers=headers)
-# print(response.json())  # {'message': 'Hello, example@example.com!'}
+headers = {'Authorization': f'Bearer {access_token}'}
+response = requests.get('http://127.0.0.1:8080/protected', headers=headers)
+print(response.json())  # {'message': 'Hello, example@example.com!'}
