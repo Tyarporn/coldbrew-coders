c7ad8918a2c8a31c0f3d4eeff30b6c0e0c062aa1
Tahmeed Chowdhury <31861714+chowdhurytahmeed@users.noreply.github.com>
2023-04-18 10:55:53 -0400

    Update crisco_dev.py

--- bot_development/crisco_dev/crisco_dev.py
+++ bot_development/crisco_dev/crisco_dev.py
@@ -1,12 +1,15 @@
 # import endpoints as ep
 import discord
 import os
+import asyncio
+import requests
 # from dotenv import load_dontenv
 from discord.ext import commands
 
 # load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+client = discord.Client()
 
 intents = discord.Intents.all()
 crisco = commands.Bot(intents=intents, command_prefix="!")
@@ -16,3 +19,22 @@ async def on_ready():
     for guild in crisco.guilds:
         if guild.name == GUILD:
             break
+
+async def on_ready():
+    print("User {0.user}".format(client))
+
+async def send_live_crisco():
+    while True:
+        response = requests.get('https://cricapi.com/api/matches?apikey=your_api_key')
+        data = response.json()
+
+        message = ''
+        for match in data['matches']:
+            message += f"{match['team-1']} vs {match['team-2']}: {match['score']}\n"
+
+        channel = client.get_channel(your_channel_id)
+        await channel.send(message)
+
+        await asyncio.sleep(60)
+
+client.run('your_bot_token')
