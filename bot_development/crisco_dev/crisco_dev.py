b78f95a00dbf0921ca08f9d7bb52fcffc1cfb00c
Tahmeed Chowdhury <31861714+chowdhurytahmeed@users.noreply.github.com>
2023-04-18 10:58:31 -0400

    Update crisco_dev.py

--- bot_development/crisco_dev/crisco_dev.py
+++ bot_development/crisco_dev/crisco_dev.py
@@ -27,14 +27,18 @@ async def send_live_crisco():
     while True:
         response = requests.get('https://cricapi.com/api/matches?apikey=your_api_key')
         data = response.json()
+        #gets cricket score from API
 
         message = ''
         for match in data['matches']:
             message += f"{match['team-1']} vs {match['team-2']}: {match['score']}\n"
+        #parses the data and puts it in message form
 
         channel = client.get_channel(your_channel_id)
         await channel.send(message)
+        #message is sent to discord server
 
         await asyncio.sleep(60)
+        #update every minute for next live score
 
 client.run('your_bot_token')
