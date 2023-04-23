79fa29407a5156ac6886da39a209bb2be90f6735
Ty Arpornsuksant <tyler.arporn741@gmail.com>
2023-04-23 13:42:38 -0400

    flake8 errors for crisco

--- bot_development/crisco_dev/crisco_dev.py
+++ bot_development/crisco_dev/crisco_dev.py
@@ -14,31 +14,36 @@ client = discord.Client()
 intents = discord.Intents.all()
 crisco = commands.Bot(intents=intents, command_prefix="!")
 
-@crisco.event
-async def on_ready():
-    for guild in crisco.guilds:
-        if guild.name == GUILD:
-            break
+
+# @crisco.event
+# async def on_ready():
+#     for guild in crisco.guilds:
+#         if guild.name == GUILD:
+#             break
+
 
 async def on_ready():
     print("User {0.user}".format(client))
 
+
 async def send_live_crisco():
+    crisco_url = 'https://cricapi.com/api/matches?apikey=your_api_key'
     while True:
-        response = requests.get('https://cricapi.com/api/matches?apikey=your_api_key')
+        response = requests.get(crisco_url)
         data = response.json()
-        #gets cricket score from API
+        # gets cricket score from API
 
         message = ''
         for match in data['matches']:
             message += f"{match['team-1']} vs {match['team-2']}: {match['score']}\n"
-        #parses the data and puts it in message form
+        # parses the data and puts it in message form
 
+        your_channel_id = None  # PLACEHOLDER FOR FLAKE8  - Ty
         channel = client.get_channel(your_channel_id)
         await channel.send(message)
-        #message is sent to discord server
+        # message is sent to discord server
 
         await asyncio.sleep(60)
-        #update every minute for next live score
+        # update every minute for next live score
 
 client.run('your_bot_token')
